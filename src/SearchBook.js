a5bb8373ac3c66b77db3aec6c1458498d7e75a13
Andrei Kuleuski <kuljok@gmail.com>
2022-09-11 16:21:54 +0300

    Implement Debounce for the search

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -3,13 +3,26 @@ import {Box, Text, HStack, VStack, Input, IconButton,
 import {Link} from 'react-router-dom';
 import {ArrowBackIcon} from '@chakra-ui/icons';
 import {search} from './api/BooksAPI.js';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Book from './Book.js';
 import PropTypes from 'prop-types';
+import {useDebounce} from './useDebounce.js'
 
 const SearchBook = ({mapBook}) => {
 
   const [found, setFound] = useState([]);
+
+  let handler = null;
+
+  const debounceSearch = (searchText) => {
+    if (handler)
+    {
+      clearTimeout(handler);
+      handler = null;
+    }
+
+    handler = setTimeout(() => updateSearch(searchText), 500);
+  }
 
   const updateSearch = async (searchText) => {
     const searchResults = await search(searchText, 20);
@@ -30,7 +43,7 @@ const SearchBook = ({mapBook}) => {
           <IconButton variant="ghost" icon={<ArrowBackIcon w={8} h={8}/>} />
         </Link>
         <Input placeholder="Search by title, author or ISBN" 
-            onChange={(event) => updateSearch(event.target.value)}/>
+            onChange={(event) => debounceSearch(event.target.value)}/>
       </HStack>
       <VStack>
       <SimpleGrid columns={{sm: 2, md: 3, lg: 5}} spacing={10} > 
